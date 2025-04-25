7234e2004e73b44453c2659c709bc9a63539ddd7
logikmars <dima76673@gmail.com>
2025-04-25 17:51:05 +0300

    fix

--- src/assets/components/Btn/Btn.jsx
+++ src/assets/components/Btn/Btn.jsx
@@ -3,7 +3,7 @@ export default ({ onclick, click, title}) => {
 
     return (
         <div className='Btn_border'>
-            <div className={`Btn ${click === 3 ? 'Btn_disabled' : ''}`} onClick={onclick} style={click === 3 ? { background: 'linear-gradient(to bottom, #D9D9D9, #D9D9D9)' } : {}}
+            <div className={`Btn ${click === 3 ? 'Btn_disabled' : ''}`} onClick={onclick}
             disabled={click === 3}
             >
                 {title}
