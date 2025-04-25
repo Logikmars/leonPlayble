0093d0215964f3da0bcce5bdbe7118f65e09b934
ivandalechenko <ivandalechenko@gmail.com>
2025-04-25 13:25:12 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,30 @@ import GreenLabel from './assets/components/GreenLabel/GreenLabel';
 function App() {
   return (
     <div className="App">
-        <div className="App_title">
-          <img src="/img/title.webp" alt="" />
-        </div>
-        <div className='App_decorBallRight free_img'>
-          <img src="/img/rightJackpot.webp" alt="" />
-        </div>
-        <div className='App_coinVolcano'>
-          <img src="/img/coinVolcano.webp" alt="" />
-        </div>
-        <div className='App_green'>
-          <GreenLabel />
-        </div>
-        <div className='App_table'>
-          <img src="/img/table.webp" alt="" />
-        </div>
-        <div className='App_btn'>
-          <Btn />
-        </div>
-        <div className='App_decorBallLeft free_img'>
-          <img src="/img/leftJackpot.webp" alt="" />
-        </div>
-        <div className='App_decorFlame free_img'>
-          <img src="/img/flame.webp" alt="" />
-        </div>
+      <div className="App_title free_img">
+        <img src="/img/title.webp" alt="" />
+      </div>
+      <div className='App_decorBallRight free_img'>
+        <img src="/img/rightJackpot.webp" alt="" />
+      </div>
+      <div className='App_coinVolcano free_img'>
+        <img src="/img/coinVolcano.webp" alt="" />
+      </div>
+      <div className='App_green free_img'>
+        <GreenLabel />
+      </div>
+      <div className='App_table free_img'>
+        <img src="/img/table.webp" alt="" />
+      </div>
+      <div className='App_btn free_img'>
+        <Btn />
+      </div>
+      <div className='App_decorBallLeft free_img'>
+        <img src="/img/leftJackpot.webp" alt="" />
+      </div>
+      <div className='App_decorFlame free_img'>
+        <img src="/img/flame.webp" alt="" />
+      </div>
     </div>
   )
 }
